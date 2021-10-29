549106927122b99363f4f39164ce3e022d9be14a
Char1ee <xingjianli59@gmail.com>
2021-10-29 23:22:17 +0800

    update 627

---
+++ SQL/627.SwapSalary.sql
@@ -0,0 +1,14 @@
+-- Using CASE...WHEN
+UPDATE Salary SET sex =
+    CASE sex
+    WHEN 'm' THEN
+    'f'
+    WHEN 'f' THEN
+    'm' end;
+
+-- Using IF()
+UPDATE Salary SET sex = if(sex ='m', 'f', 'm');
+
+-- Using ASCII()
+Update Salary set sex = (ASCII('m') + ASCII('f') - ASCII(sex));
+Update Salary set sex = (ASCII('m') ^ ASCII('f') ^ ASCII(sex));
