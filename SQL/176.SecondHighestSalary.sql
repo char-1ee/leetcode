8d3e76075aef00ddea0298c22fd61787a6198b85
Char1ee <xingjianli59@gmail.com>
2021-10-30 22:27:32 +0800

    update 176

---
+++ SQL/176.SecondHighestSalary.sql
@@ -0,0 +1,20 @@
+-- Method 1
+SELECT DISTINCT max(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT max(salary) FROM Employee);
+
+-- Method 2 
+SELECT 
+    (SELECT DISTINCT salary FROM Employee 
+    ORDER BY  salary DESC limit 1, 1) AS SecondHighestSalary;
+
+-- Method 3
+SELECT ifnull(
+    (SELECT DISTINCT salary
+    FROM employee
+    ORDER BY  salary DESC limit 1, 1), null) AS SecondHighestSalary;
+
+-- Method 4
+SELECT max(a.Salary) AS SecondHighestSalary
+FROM Employee a
+JOIN Employee b ON a.Salary < b.Salary;
