d967c34e9eec32ad7a6593e58f614a971bd57ccb
Char1ee <xingjianli59@gmail.com>
2021-11-05 17:22:28 +0800

    update 509

---
+++ DP/509.FibonacciNumber.java
@@ -0,0 +1,18 @@
+package DP;
+
+public class FibonacciNumber {
+    public int fib(int n) {
+        if (n <= 1)
+            return n;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {      // Start from i=2 to i=n (instead of i=n-1)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];                       // return n-th element
+    }
+}
