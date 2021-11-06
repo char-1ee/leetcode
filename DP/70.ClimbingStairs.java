7b229685d7fb0d6e22ce93f094738a63dceefe42
Char1ee <xingjianli59@gmail.com>
2021-11-06 16:40:13 +0800

    Add solution for 70

--- DP/70.ClimbingStairs.java
+++ DP/70.ClimbingStairs.java
@@ -1,7 +1,7 @@
 package DP;
 
 class Solution {
-    public itn climbStairs(int n) {
+    public int climbStairs(int n) {
         if (n <= 1) return n;
         int[] dp = new int[n+1];
 
@@ -9,9 +9,27 @@ class Solution {
         dp[1] = 1;
         dp[2] = 2;
 
-        if (int i = 3; i <= n; i++)
+        for (int i = 3; i <= n; i++) 
             dp[i] = dp[i-1] + dp[i-2];
 
         return dp[n];
+    }
+
+    /**
+     * DP optimized
+     */
+    public int climbStairs2(int n) {
+        if (n <= 1)
+            return n;
+        int prev1 = 1;
+        int prev2 = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int new_value = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = new_value;
+        }
+
+        return prev2;
     }
 }
