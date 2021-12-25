61363592d81a47b7d6e28da1506f6d7e47d5a68b
char-1ee <xingjianli59@gmail.com>
2021-12-25 23:45:49 +0800

    Add 234

---
+++ 234.PalindromeLinkedList.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    /** Time: O(n); Space: O(1)*/
+    bool isPalindrome(ListNode* head) {
+        if (!head->next)    return true;
+        
+        // Step 1: find middle node
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        // Step 2: reverse the list after middle node
+        ListNode *last = slow->next, *pre = head;
+        while (last->next) {
+            ListNode *tmp = last->next;
+            last->next = tmp->next;
+            tmp->next = slow->next;
+            slow->next = tmp;
+        }
+
+        // Step 3: compare the nodes of two lists ([0..mid] and [mid+1..end])
+        while (slow->next) {
+            slow = slow->next;
+            if (pre->val != slow->val) return false;
+            pre = pre->next;
+        }
+        return true;
+    }
+};
