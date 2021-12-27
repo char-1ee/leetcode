8ee501b3bc85bbb2f32c2957a03456fc761c33ec
char-1ee <xingjianli59@gmail.com>
2021-12-27 11:18:45 +0800

    Add 476 java

---
+++ 476.NumberComplement.java
@@ -0,0 +1,20 @@
+class Solution {
+    int numComplement(int num) {
+        int cnt = 0;
+        int tmp = 0;
+        while (tmp < num) {
+            tmp += Math.pow(2, cnt);
+            cnt++;
+        }
+        return tmp - num;  
+    }
+
+    /** Alternative */
+    int numComplement2(int num) {
+        int n = 0;
+        while (n < num) {
+            n = (n << 1) | 1;
+        }
+        return n - num;
+    }
+}
