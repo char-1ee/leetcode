6ad2415a1a91d2d8e3a75db39e7aa72af396c14e
char-1ee <xingjianli59@gmail.com>
2021-12-31 14:06:02 +0800

    Add 94

---
+++ BinaryTree/94.BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    /** Iterative */
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        vector<int> res;
+        TreeNode* p = root;
+
+        while (!p || !s.empty()) {
+            if (!p) {
+                s.push(p);
+                p = p->left;
+            } else {
+                p = s.top(); s.pop();
+                res.push_back(p->val);
+                p = p->right;
+            }
+        }
+        return res;
+    }
+
+    /** Recursive */
+    vector<int> res;
+    vector<int> inorderTraversalRe(TreeNode* root) {
+        inHelper(root);
+        return res;
+    }
+
+    void inHelper(TreeNode* p) {
+        if (!p) return;
+        inHelper(p->left);
+        res.push_back(p->val);
+        inHelper(p->right);
+    }
+}
