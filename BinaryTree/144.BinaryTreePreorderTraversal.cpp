ed6e64a4e4bf875279c668f5d2cfd02752f80537
char-1ee <xingjianli59@gmail.com>
2021-12-31 13:38:31 +0800

    Add 144

---
+++ BinaryTree/144.BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    /** Iterative with stack */
+    vector<int> preorderTraversal(TreeNode* root) {
+        if (!root) return {}
+        
+        stack<TreeNode*> s;
+        vector<int> res;
+        s.push(root);
+
+        while (!s.empty()) {
+            TreeNode* p = s.top(); s.pop();
+            res.push_back(p->val);
+            if (p->right) s.push(p->left);
+            if (p->left) s.push(p->right);
+        }
+        return res;
+    } 
+
+    /** Recursive method 1 */
+    vector<int> res;
+
+    vector<int> preorderTraversalRe1(TreeNode* root) {
+        preHelper(root);
+        return res;
+    }
+    void preHelper(TreeNode* root) {
+        if (!root) return;
+        res.push_back(root->val);
+        preHelper(root->left);
+        preHelper(root->right);
+    }
+
+    /** Recursive method 2 */
+    vector<int> preorderTraversalRe2(TreeNode* root) {
+        if (!root) return;
+        re.push_back(root->val);
+        preorderTraversalRe2(root->left);
+        preorderTraversalRe2(root->right);
+        return res;
+    }
+};
