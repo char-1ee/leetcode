54f14d1be7d54edcbe53ea6a377af43ea60d3e72
char-1ee <xingjianli59@gmail.com>
2022-01-04 20:49:14 +0800

    Update 141

--- LinkedList/141.LinkedListCycle.java
+++ LinkedList/141.LinkedListCycle.java
@@ -1,5 +1,16 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
 class Solution {
-    /** Fast-pointer and Slow-pointer */ 
+    /** Fast-pointer and Slow-pointer */
     public boolean hasCycle(ListNode head) {
         ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
@@ -13,20 +24,14 @@ class Solution {
 
     /** Recursion: but it BREAKS the original ll structure! */
     public boolean hasCycle2(ListNode head) {
-        if (head == null || head.next == null) return false;
-        if (head.next == head) return true;
+        if (head == null || head.next == null)
+            return false;
+        if (head.next == head)
+            return true;
 
         ListNode nextNode = head.next;
         head.next = head;
-        boolean isCycle = hasCycle2(nextNode);  // Alternative for iteration : recursion
+        boolean isCycle = hasCycle2(nextNode); // Alternative for iteration : recursion
         return isCycle;
-    }
-}
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
-        val = x;
-        next = null;
     }
 }
