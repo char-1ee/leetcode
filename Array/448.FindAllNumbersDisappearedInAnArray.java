f46791456c0b564d8a7adb52b9efb1091fcbee17
char-1ee <xingjianli59@gmail.com>
2022-01-05 22:42:35 +0800

    Update 448

--- Array/448.FindAllNumbersDisappearedInAnArray.java
+++ Array/448.FindAllNumbersDisappearedInAnArray.java
@@ -1,16 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        
+
         int n = nums.length;
         List<Integer> res = new ArrayList<>();
         int[] list = new int[n];
-        
+
         for (int num : nums) {
             list[num - 1]++;
         }
         for (int i = 0; i < n; i++) {
             if (list[i] == 0)
-                res.add(i+1);
+                res.add(i + 1);
         }
         return res;
     }
