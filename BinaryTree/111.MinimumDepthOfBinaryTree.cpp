0b7c22c471ff495d3d107837b7efc7ba0e9db5a8
char-1ee <xingjianli59@gmail.com>
2022-01-06 20:49:32 +0800

    Add 111

---
+++ BinaryTree/111.MinimumDepthOfBinaryTree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) return 0;
+        
+        // be careful not to count the depth of dead ends. 
+        // Imagine a completely unbalanced tree i.e. no node has left children. 
+        // Then the minimum depth should not consider going left from the root to be a complete path
+        // because it did not reach a leaf. So we only recurse on a child if it's not null.
+        if (!root->left) return minDepth(root->right) + 1;
+        if (!root->right) return minDepth(root->left) + 1;
+        return min(minDepth(root->left), minDepth(root->right)) + 1;
+    }
+};
