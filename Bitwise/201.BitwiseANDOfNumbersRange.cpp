8379da71432aeeaabbd7c21fefe31b28fc40048c
char-1ee <xingjianli59@gmail.com>
2022-01-06 19:41:12 +0800

    Add 201

---
+++ Bitwise/201.BitwiseANDOfNumbersRange.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rangeBitwiseAnd1(int left, int right) {
+        int i = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            i++;
+        }
+        return left << i;
+    }
+
+    int rangeBitwiseAnd2(int m, int n) {
+        unsigned int mask = UINT_MAX;
+        while ((m & mask) != (n & mask)) {
+            mask <<= 1;
+        }
+        return m & mask;
+    }
+};
