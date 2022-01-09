1f7b4eea721faf30bb656976c4254445562afa35
char-1ee <xingjianli59@gmail.com>
2022-01-09 14:24:53 +0800

    Add 117

---
+++ BinaryTree/117.PopulatingNextRightPointersInEachNode.cpp
@@ -0,0 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    /** iterative */
+    Node* connect(Node* root) {
+        if (!root) return NULL;
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int len = q.size();
+            for (int i = 0; i < len; ++i) {
+                Node* t = q.front(); q.pop();
+                if (i < len - 1) t->next = q.front();
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+        }
+        return root;
+    }
+
+    /** recursive */
+    Node* connectRe(Node* root) {
+        if (!root) return nullptr;
+
+        Node* p = root->next;
+        while (p) {
+            if (p->left) {
+                p = p->left;
+                break;
+            }
+            if (p->right) {
+                p = p->right;
+                break;
+            }
+            p = p->next;
+        }
+        if (root->left) root->left->next = root->right;
+        if (root->right) root->right->next = (root->next == NULL) ? NULL : root->next->left;
+        connect(p->left);
+        connect(p->right);
+        return root;
+    }
+};
