511960507cbfa63b2acd01f9fd186b5f1719dec7
char-1ee <xingjianli59@gmail.com>
2022-01-11 22:56:43 +0800

    Update 1

--- Elementary/1.TwoSum.java
+++ Elementary/1.TwoSum.java
@@ -1,5 +1,8 @@
 package Elementary;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     /** Brute-force */
     public int[] twoSum(int[] nums, int target) {
@@ -11,5 +14,16 @@ class Solution {
             }
         }
         throw new IllegalArgumentException("No solution");
+    }
+
+    /** Hash map */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]))
+                return new int[] { map.get(target - nums[i]), i };
+            map.put(nums[i], i);
+        }
+        return null;
     }
 }
