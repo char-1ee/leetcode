3be431774e7d4387e6b92dab835e27b9432c30df
char-1ee <xingjianli59@gmail.com>
2022-01-12 11:14:03 +0800

    Add 219

--- HashTable/217.ContainsDuplicate.java
+++ HashTable/217.ContainsDuplicate.java
@@ -12,7 +12,6 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int n = nums[i];
             if (map.containsKey(nums[i]))
                 return true;
             map.put(nums[i], i);
