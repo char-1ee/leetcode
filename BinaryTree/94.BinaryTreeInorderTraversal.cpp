6bc3407f0cdfea8654c931d20aa8574dc5f8fa4f
char-1ee <xingjianli59@gmail.com>
2022-01-13 14:34:17 +0800

    Update 94

--- BinaryTree/94.BinaryTreeInorderTraversal.cpp
+++ BinaryTree/94.BinaryTreeInorderTraversal.cpp
@@ -12,7 +12,7 @@
 
 class Solution {
 public:
-    /** Iterative */
+    /** Iterative method 1*/
     vector<int> inorderTraversal(TreeNode* root) {
         stack<TreeNode*> s;
         vector<int> res;
@@ -27,6 +27,23 @@ public:
                 res.push_back(p->val);
                 p = p->right;
             }
+        }
+        return res;
+    }
+
+    /** Iterative method 2 */
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        vector<int> res;
+        TreeNode* p = root;
+        while (p || !s.empty()) {
+            while (p) {
+                s.push(p);
+                p = p->left;
+            }
+            p = s.top(); s.pop();
+            res.push_back(p->val);
+            p = p->right;
         }
         return res;
     }
