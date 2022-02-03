8357cff7100d996b5c9d49d4f41a9abb3247150b
char-1ee <xingjianli59@gmail.com>
2022-02-03 17:18:01 +0800

    Update 206

--- LinkedList/206.ReverseLinkedList.java
+++ LinkedList/206.ReverseLinkedList.java
@@ -1,25 +1,52 @@
+package LinkedList;
+
 class ListNode {
-   int val;
-   ListNode next;
-   ListNode() {}
-   ListNode(int val) { this.val = val; }
-   ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
 
 class Solution {
     /** Iterative */
     public ListNode reverseLinkedList(ListNode head) {
-        if (head == null) return null;
+        if (head == null)
+            return null;
         ListNode prev = null;
         ListNode curr = head;
 
         while (curr != null) {
-            ListNode nextTemp = curr.next;  // Save next node
+            ListNode nextTemp = curr.next; // Save next node
             /** FIGURE OUT */
-            curr.next = prev;   // Reverse (first time prev = null) 
-            prev = curr;    // Used for next iteration
-            curr = nextTemp;    // Move to next node
+            curr.next = prev; // Reverse (first time prev = null)
+            prev = curr; // Used for next iteration
+            curr = nextTemp; // Move to next node
         }
         return prev;
-    } 
+    }
+
+    /** recursive 1 */
+    public ListNode reverseList(ListNode head) {
+        return reverseList(head, null);
+    }
+
+    private ListNode reverseList(ListNode curr, ListNode prev) {
+        if (curr == null)
+            return prev;
+        ListNode next = curr.next;
+        curr.next = prev;
+        return reverseList(next, curr);
+    }
 }
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/discuss/440696/Easy-to-understand-recursive-solution-(-beats-98-takes-2-arguments-)
+ * 
+ */
