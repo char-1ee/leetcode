4d1135bc3449975adecef930ad9f19fe454d8600
char-1ee <xingjianli59@gmail.com>
2022-02-04 13:17:02 +0800

    update solutions

--- DPAndRecursion/1179.NthTribonacciNumber.java
+++ DPAndRecursion/1179.NthTribonacciNumber.java
@@ -1,7 +1,7 @@
-package DP;
+package DPAndRecursion;
 
 class NthTribonacciNumber {
-    
+
     /* DP Method 1 */
     public int tribonacci1(int n) {
         if (n == 0)
@@ -22,17 +22,18 @@ class NthTribonacciNumber {
 
     /* DP Method 2: Amazing */
     public int tribonacci2(int n) {
-        int dp[] = {0, 1, 1};
+        int dp[] = { 0, 1, 1 };
         for (int i = 3; i <= n; ++i)
             dp[i % 3] = dp[0] + dp[1] + dp[2];
         return dp[n % 3];
     }
 
     /**
-     *  https://leetcode.com/problems/n-th-tribonacci-number/discuss/345236/JavaC%2B%2BPython-Easy-and-Concise
+     * https://leetcode.com/problems/n-th-tribonacci-number/discuss/345236/JavaC%2B%2BPython-Easy-and-Concise
      */
     public int tribonacci3(int n) {
-        if (n < 2) return n;
+        if (n < 2)
+            return n;
         int a = 0, b = 1, c = 1, d;
         while (n-- > 2) {
             d = a + b + c;
--- DPAndRecursion/746.MinCostClimbingStairs.java
+++ DPAndRecursion/746.MinCostClimbingStairs.java
@@ -1,23 +1,23 @@
-package DP;
+package DPAndRecursion;
 
 class MinCostClimbingStairs {
     public int minCostClimbingStairs1(int[] cost) {
         int n = cost.length;
-        int[] dp = new int[n];      // dp[i] stands for min cost when reach (i+1)th
-        
+        int[] dp = new int[n]; // dp[i] stands for min cost when reach (i+1)th
+
         dp[0] = cost[0];
         dp[1] = cost[1];
-        
-        for (int i=2; i<n; i++){
-            dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
         }
-        
-        return Math.min(dp[n-1], dp[n-2]);  // there is no cost[n] for top floor
+
+        return Math.min(dp[n - 1], dp[n - 2]); // there is no cost[n] for top floor
     }
 
-    public int minCostClimbingStairs2(int[] cost){
+    public int minCostClimbingStairs2(int[] cost) {
         int a = 0, b = 0;
-        for (int num : cost){
+        for (int num : cost) {
             int temp = Math.min(a, b) + num;
             a = b;
             b = temp;
