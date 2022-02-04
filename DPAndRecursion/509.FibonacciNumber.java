3c94c442e222e9409c854047f5078f3cf7bc5dff
char-1ee <xingjianli59@gmail.com>
2022-02-04 10:17:19 +0800

    Update 509

--- DPAndRecursion/509.FibonacciNumber.java
+++ DPAndRecursion/509.FibonacciNumber.java
@@ -1,7 +1,11 @@
-package DP;
+package DPAndRecursion;
+
+import java.util.HashMap;
 
 class FibonacciNumber {
-    public int fib(int n) {
+
+    /** DP */
+    public int fibDP(int n) {
         if (n <= 1)
             return n;
 
@@ -9,10 +13,33 @@ class FibonacciNumber {
         dp[0] = 0;
         dp[1] = 1;
 
-        for (int i = 2; i <= n; i++) {      // Start from i=2 to i=n (instead of i=n-1)
+        for (int i = 2; i <= n; i++) { // Start from i=2 to i=n (instead of i=n-1)
             dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        return dp[n];                       // return n-th element
+        return dp[n]; // return n-th element
+    }
+
+    /** Recursion */
+    public int fib(int n) {
+        if (n < 2)
+            return n;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+
+    /** recursion with cathe */
+    private HashMap<Integer, Integer> cathe = new HashMap<>();
+
+    public int fibmemo(int n) {
+        if (cathe.containsKey(n))
+            return cathe.get(n);
+        int res;
+        if (n < 2)
+            return n;
+        else
+            res = fibmemo(n - 1) + fibmemo(n - 2);
+        cathe.put(n, res);
+        return res;
     }
 }
