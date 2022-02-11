8a2f35736255cc36af8f537556b3e6854b41cff7
char-1ee <xingjianli59@gmail.com>
2022-02-11 14:07:00 +0800

    Add 1507

---
+++ 1507.ReformatDate.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reformatDate(String date) {
+        StringBuilder sb = new StringBuilder();
+        Map<String, String> m = new HashMap<>(){{
+            put("Jan", "01");
+            put("Feb", "02");
+            put("Mar", "03");
+            put("Apr", "04");
+            put("May", "05");
+            put("Jun", "06");
+            put("Jul", "07");
+            put("Aug", "08");
+            put("Sep", "09");
+            put("Oct", "10");
+            put("Nov", "11");
+            put("Dec", "12");
+        }};
+        
+        String[] strs = date.split("\\s");
+        sb.append(strs[2]).append("-");
+        sb.append(m.get(strs[1])).append("-");
+        if (strs[0].length() < 4) {
+            sb.append("0").append(strs[0].substring(0, 1));
+        } else {
+            sb.append(strs[0].substring(0, 2));
+        }
+        return sb.toString();
+    }
+}
---
+++ BinarySearchTree/230.KthSmallestElementInaBST.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> s = new Stack<>();
+        while (root != null || !s.isEmpty())
+        {
+            while (root != null)
+            {
+                s.push(root);
+                root = root.left;
+            }
+            root = s.pop();
+            k--; // every time k--, means a leftmost leaf node is counted
+            if (k == 0) break; // when kth leftmost leaf node is met, end loop
+            root = root.right;
+        }
+        return root.val;
+    }
+}
