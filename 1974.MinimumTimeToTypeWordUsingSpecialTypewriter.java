b0176f1b6e828a10d9fbfc83ac6a51b074f365cb
char-1ee <xingjianli59@gmail.com>
2022-02-20 23:43:31 +0800

    Add 1974

---
+++ 1974.MinimumTimeToTypeWordUsingSpecialTypewriter.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minTimeToType(String word) {
+        int len = word.length();
+        int res = 0;
+        char c = 'a';
+        for (int i = 0; i < len; i++) {
+            char c2 = word.charAt(i);
+            int dist = Math.abs(c2 - c);
+            res += Math.min(dist, 26 - dist);
+            c = c2;
+        }
+        return res + len;
+    }
+}
