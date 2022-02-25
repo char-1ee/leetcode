5450038b6191a30fe69a41304b504edcfd35ac44
char-1ee <xingjianli59@gmail.com>
2022-02-25 22:34:14 +0800

    Add 69

---
+++ BinarySearch/69.Sqrt(x).cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    // sqrt(x) = pow(e, 0.5 * ln(x))
+    int sqrt(int x) {
+        if (x < 2) return x;
+        int left = (int) exp(0.5 * log(x));
+        int right = left + 1;
+        return right * right > x ? left : right;
+    }
+
+    // binary search
+    int sqrt(int x) {
+        if (x < 2) int return x;
+        int left = 0, right = x / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long num = (long)mid * mid;
+            if (num == x) return mid;
+            else if (num > x) right = mid - 1;
+            else left = mid + 1;
+        }
+        return right;
+    }
+}
