2d96f500ba8d448b47ff268e6d8631974ba3c024
char-1ee <xingjianli59@gmail.com>
2022-03-01 01:42:28 +0800

    Update lower_bound cpp

--- BinarySearch/Implementations/upper_bound.cpp
+++ BinarySearch/Implementations/upper_bound.cpp
@@ -19,3 +19,25 @@ int find(vector<int>& nums, int target) {
     }
     return right - 1;
 }
+
+// About lower_bound and upper_bound
+int main()
+{
+    int gfg[] = { 5, 6, 7, 7, 6, 5, 5, 6 };
+    vector<int> v(gfg, gfg + 8); // 5 6 7 7 6 5 5 6
+    sort(v.begin(), v.end()); // 5 5 5 6 6 6 7 7
+ 
+    vector<int>::iterator lower, upper;
+    // lower_bound returns an iterator pointing to the first element in the range [first,last) which has a value not less than ‘val’. 
+    lower = lower_bound(v.begin(), v.end(), 6); // index 3 (from 0)
+
+    // upper_bound returns an iterator pointing to the first element in the range [first,last) which has a value greater than ‘val’. 
+    upper = upper_bound(v.begin(), v.end(), 6); // index 6 (from 0)
+ 
+    cout << "lower_bound for 6 at position "
+         << (lower - v.begin() + 1) << '\n';
+    cout << "upper_bound for 6 at position "
+         << (upper - v.begin() + 1) << '\n';
+ 
+    return 0;
+}
