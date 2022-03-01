da24a3b3ab1221f7721e30bc8ad82109aac382e0
char-1ee <xingjianli59@gmail.com>
2022-03-01 11:24:03 +0800

    Add 470

---
+++ mathematic/470.ImplementRand10UsingRand7.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    // reject sampling
+    int rand10() {
+        while (true) {
+            int num = (rand7() - 1) * 7 + rand7();
+            if (num <= 40) return num % 10 + 1;
+        }
+    }
+
+    // reduce reject sampling probability
+    int rand10() {
+        while (true) {
+            int a = rand7(), b = rand7();
+            int num = (a-1) * 7 + b;
+            if (num <= 40) return num % 10 + 1;
+            a = num - 40, b = rand7();
+            num = (a-1) * 7 + b;
+            if (num <= 60) return num % 10 + 1;
+            a = num - 60, b = rand7();
+            num = (a - 1) * 7 + b;
+            if (num <= 20) return num % 10 + 1;
+        }
+    }
+};
+
+/**
+ * https://www.cnblogs.com/grandyang/p/9727206.html
+ * Reject random number into a table T(m, n)
+ * table T = (randN() - 1) * M + randN() 
+ */
