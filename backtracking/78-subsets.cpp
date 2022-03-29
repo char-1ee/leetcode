d487707003d193b2c2addf05c5f3169ac73ef92e
char-1ee <xingjianli59@gmail.com>
2022-03-29 14:18:37 +0800

    Add solution

--- backtracking/78-subsets.cpp
+++ backtracking/78-subsets.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
 
-    // Backtracking
-    vector<vector<int>> subsets(vector<int>& nums) {
+    // Backtracking (differ from traditional backtrack, more like trees' recursion)
+    // refer to https://www.cnblogs.com/TenosDoIt/p/3451902.html
+    // the inserting order: 
+    // [], [1], [1 2], [1 2 3], [1 3], [2], [2 3], [3]
+    vector<vector<int>> subsets1(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> list;
         backtrack(list, res, nums, 0);
@@ -21,7 +24,7 @@ public:
     }
 
     // Casscading
-    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> subsets2(vector<int>& nums) {
         vector<vector<int>> subs = {{}};
         for (int num : nums) {
             int n = subs.size(); // must fetch out the lenth, otherwise thread unsafty
@@ -31,7 +34,26 @@ public:
             }
         }
         return subs;
-    }
+    }    
 
+    // Lexicographic (Binary Sorted) Subsets
+    vector<vector<int>> subsets3(vector<int>& nums) {
+        vector<vector<int>> res;
+        int max = 1 << nums.size();
+        for (int i = 0; i < max; i++) {
+            vector<int> num = mask(nums, i);
+            res.push_back(num);
+        }
+        return res;
+    }
     
+    vector<int> mask(vector<int>& nums, int k) {
+        vector<int> sub; 
+        for (int idx = 0, i = k; i > 0; i >>= 1, idx++) {
+            if (i & 1 == 1) {
+                sub.push_back(nums[idx]);
+            }
+        }
+        return sub;
+    }
 };
