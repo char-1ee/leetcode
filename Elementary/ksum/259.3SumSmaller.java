0e29d6f0d46f68cd957b445354616ad6836cd1f5
char-1ee <xingjianli59@gmail.com>
2022-04-08 04:48:55 +0800

    Add solution

---
+++ Elementary/ksum/259.3SumSmaller.java
@@ -0,0 +1,27 @@
+package elementary.ksum;
+
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        int cnt = 0, n = nums.length;
+        Arrays.sort(nums);
+        if (n < 3 || 3 * nums[0] >= target)
+            return cnt;
+        for (int k = 0; k < n - 2; k++) {
+            // no need to remove duplicate coz cnt is counted by indexes
+            // if (k > 0 && nums[k] == nums[k+1]) continue;
+            int i = k + 1, j = n - 1;
+            while (i < j) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < target) {
+                    cnt += j - i;
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+        return cnt;
+    }
+}
