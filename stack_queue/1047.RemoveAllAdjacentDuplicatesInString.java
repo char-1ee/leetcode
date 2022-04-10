024db17390752fc9e70f4f667a6562d662d21206
char-1ee <xingjianli59@gmail.com>
2022-04-10 19:32:05 +0800

    Add solution

---
+++ stack_queue/1047.RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+class Solution {
+    public String removeDuplicates(String str) {
+        Stack<Character> s = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (s.empty() || c != s.peek()) {
+                s.push(c);
+            } else {
+                s.pop();
+            }
+        }
+
+        String res = "";
+        while (!s.empty()) {
+            res = s.pop() + res;
+        }
+        return res;
+    }
+}
