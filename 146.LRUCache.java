216d9f177215bd9d35584b3be784b85991fd5052
char-1ee <xingjianli59@gmail.com>
2022-04-12 11:50:15 +0800

    Add solution

--- 146.LRUCache.java
+++ 146.LRUCache.java
@@ -1,97 +1,84 @@
 import java.util.HashMap;
-import java.util.Map;
 
 class LRUCache {
-    
-    class DListNode {
-        int key;
-        int value;
-        DListNode prev;
-        DListNode next;
-        
-        DListNode() {
-            this.key = 0;
-            this.value = 0;
-            this.prev = null;
-            this.next = null;
-        }
-        
-        DListNode(int key, int value) {
-            this.key = key;
-            this.value = value;
-            this.prev = null;
-            this.next = null;
-        }
-    }    
-    
-    private Map<Integer, DListNode> m;
-    private final int limit;
-    private int count;
-    private DListNode head, tail;
 
-    public LRUCache(int capacity) {
-        this.limit = capacity;
-        this.count = 0;
-        this.m = new HashMap<>();
-        this.head = new DListNode();
-        this.tail = new DListNode();
-        head.next = tail;
-        tail.prev = head;
+    class LinkedNode {
+        int key;
+        int val;
+        LinkedNode prev;
+        LinkedNode next;
     }
     
-    public int get(int key) {
-        if (m.containsKey(key)) {
-            DListNode node = m.get(key);
-            swim(node);
-            return node.value;
-        } else 
-            return -1;
-    }
-    
-    public void put(int key, int value) {
-        DListNode node = m.get(key);
-        if (node == null) {
-            DListNode n = new DListNode(key, value);
-            add(node);
-            count++;
-            if (count > limit) {
-                m.remove(pop());
-                count--;
-            } 
-            m.put(key, node);
-        } else {
-            node.value = value;
-            swim(node);
-        }
-    }
-    
-    /** Make a recently used node swim to front */
-    private void swim(DListNode node) {
-        add(node);
-        remove(node);
-    }
-    
-    /** Add new coming node in front */
-    private void add(DListNode node) {
+    /** add a node in head */
+    private void addNode(LinkedNode node) {
         node.prev = head;
         node.next = head.next;
         head.next.prev = node;
         head.next = node;
     }
     
-    /** Remove certain node from list */
-    private void remove(DListNode node) {
-        DListNode prevNode = node.prev;
-        DListNode nextNode = node.next;
+    /** remove a node */
+    private void removeNode(LinkedNode node) {
+        LinkedNode prevNode = node.prev;
+        LinkedNode nextNode = node.next;
         prevNode.next = nextNode;
         nextNode.prev = prevNode;
+    } 
+    
+    /** move a used node to head */
+    private void moveToHead(LinkedNode node) {
+        removeNode(node);
+        addNode(node);
     }
     
-    /** Remove the node at tail when exceed limit */
-    private int pop() {
-        DListNode prevTail = tail.prev;
-        remove(prevTail);
-        return prevTail.key;
+    /** pop the tail node */
+    private LinkedNode popTail() {
+        LinkedNode node = tail.prev;
+        removeNode(node);
+        return node;
+    }
+    
+    private int capacity;
+    private int size;
+    private LinkedNode head, tail;
+    private HashMap<Integer, LinkedNode> cache;
+    
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.head = new LinkedNode();
+        this.tail = new LinkedNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        LinkedNode node = cache.get(key);
+        if (node == null) return -1;
+        moveToHead(node);
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+        LinkedNode node = cache.get(key);
+        if (node != null) {
+            node.val = value;
+            moveToHead(node);
+        } else {
+            LinkedNode newNode = new LinkedNode();
+            newNode.val = value;
+            newNode.key = key;
+            
+            cache.put(key, newNode);
+            addNode(newNode);
+            size++;
+            if (size > capacity) {
+                LinkedNode tail = popTail();
+                cache.remove(tail.key);
+                size--;
+            }
+        }
     }
 }
 
