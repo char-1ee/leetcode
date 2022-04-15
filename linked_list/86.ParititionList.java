c02d1c3f106d561ac25cb11a3bb725c9970ade6d
char-1ee <xingjianli59@gmail.com>
2022-04-16 04:47:18 +0800

    Add solution

---
+++ linked_list/86.ParititionList.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode beforeList = new ListNode();
+        ListNode afterList = new ListNode();
+        ListNode p1 = beforeList, p2 = afterList;
+        while (head != null) {
+            if (head.val < x) {
+                p1.next = head;
+                p1 = p1.next;
+            } else {
+                p2.next = head;
+                p2 = p2.next;
+            }
+            head = head.next;
+        }
+        p2.next = null;
+        p1.next = afterList.next;
+        return beforeList.next;
+    }
+}
