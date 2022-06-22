784d7ad4571a0adc7e2704d75095228fd31a097d
char-1ee <xingjianli59@gmail.com>
2022-06-22 23:35:48 +0800

    Add solution

---
+++ linked_list/237-Delete-Node-in-a-Linked-List.ts
@@ -0,0 +1,13 @@
+import { ListNode } from '../utils/ListNode';
+
+/**
+ * Write a function to delete a node in a singly-linked list.
+ * You will not be given access to the head of the list, 
+ * instead you will be given access to the node to be deleted directly.
+ * It is guaranteed that the node to be deleted is not a tail node in the list.
+ */
+function deleteNode(node: ListNode | null): void {
+    if (!node) return;
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
