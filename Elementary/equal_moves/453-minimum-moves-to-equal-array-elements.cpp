25ba7c2f5bde60496d0361b7d25052a73b3a333b
char-1ee <xingjianli59@gmail.com>
2022-10-25 11:26:48 +0800

    Add solution

---
+++ Elementary/equal_moves/453-minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int n = nums.size(), mn = INT_MAX, res = 0;
+        for (auto num : nums) {
+            mn = min(num, mn);
+        }
+        for (auto num : nums) {
+            res += num - mn;
+        }
+        return res;
+    }
+
+    int minMoves_solution2(vector<int>& nums) {
+        int res = 0, mn = INT_MAX;
+        for (auto num : nums) {
+            res += num;
+            mn = min(mn, num);
+        }
+        return res - mn * nums.size();
+    }
+};
