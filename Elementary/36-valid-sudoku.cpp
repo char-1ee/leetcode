8ecb6eff2f397dfd944bc45ad4b116b729bc197f
char-1ee <xingjianli59@gmail.com>
2022-11-03 18:23:09 +0800

    Add solution

--- Elementary/36-valid-sudoku.cpp
+++ Elementary/36-valid-sudoku.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& grid) {
+        // arr[idx][val]: idx stands for n-th row/col/box, val stands for appearance of certain val
         vector<vector<char>> row(9, vector<char>(9, 0));
         vector<vector<char>> col(9, vector<char>(9, 0));
         vector<vector<char>> box(9, vector<char>(9, 0));
@@ -11,12 +12,32 @@ public:
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (grid[i][j] == '.') continue;
-                int c = grid[i][j] - '1'; // val of a cell as index
-                if (row[i][c] || col[c][j] || box[3 * (i / 3) + j / 3][c])
-                    return false;
+                int c = grid[i][j] - '1'; // val
+                if (row[i][c] || col[j][c] || box[3 * (i / 3) + j / 3][c]) return false;
                 row[i][c] = 1;
-                col[c][j] = 1;
+                col[j][c] = 1;
                 box[3 * (i / 3) + j / 3][c] = 1;
+            }
+        }
+        return true;
+    }
+};
+
+// To save space, encoded press with string
+class Solution2 {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> st;
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') continue;
+                string t = "(" + to_string(board[i][j]) + ")";
+                string row = to_string(i) + t, col = t + to_string(j),
+                       cell = to_string(i / 3) + t + to_string(j / 3);
+                if (st.count(row) || st.count(col) || st.count(cell)) return false;
+                st.insert(row);
+                st.insert(col);
+                st.insert(cell);
             }
         }
         return true;
