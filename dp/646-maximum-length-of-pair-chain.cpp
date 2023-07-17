d9366e436dc2282a12a613f881040cadc46aa567
char-1ee <xingjianli59@gmail.com>
2023-07-17 17:46:12 +0800

    Add solution

---
+++ dp/646-maximum-length-of-pair-chain.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// DP: O(n^2), O(n)
+class Solution {
+   public:
+    int findLongestChain(vector<vector<int>> &pairs) {
+        int mx = 1, n = pairs.size();
+        auto comp = [](const vector<int> &a, const vector<int> &b) {
+            return a[0] < b[0];
+        };
+        sort(pairs.begin(), pairs.end(), comp);
+
+        vector<int> dp(n, 1);
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (pairs[i][1] < pairs[j][0]) {
+                    dp[i] = max(dp[j] + 1, dp[i]);
+                }
+            }
+            mx = max(dp[i], mx);
+        }
+        return mx;
+    }
+};
+
+// Greedy, O(nlogn), O(1)
+class Solution2 {
+   public:
+    int findLongestChain(vector<vector<int>> &pairs) {
+        auto comp = [](const vector<int> &a, const vector<int> &b) {
+            return a[1] < b[1];
+        };
+        sort(pairs.begin(), pairs.end(), comp);
+
+        int res = 0, curr = INT_MIN;  // curr: current tail of chain,
+        for (auto &p : pairs) {
+            if (p[0] > curr) {
+                res++;
+                curr = p[1];
+            }
+        }
+        return res;
+    }
+};
