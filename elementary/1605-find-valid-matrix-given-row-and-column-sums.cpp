433e3310b900859ba72af0804a79db8a0168b0aa
char-1ee <xingjianli59@gmail.com>
2023-08-28 00:04:29 +0800

    Add solution

---
+++ elementary/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    vector<vector<int>> restoreMatrix(vector<int> &rowSum,
+                                      vector<int> &colSum) {
+        int n = rowSum.size(), m = colSum.size();
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                ans[i][j] = min(rowSum[i], colSum[j]);
+                rowSum[i] -= ans[i][j];
+                colSum[j] -= ans[i][j];
+            }
+        }
+        return ans;
+    }
+};
